e6daf9235097bc51783e14332b5d898f5d66694c
NCCONG <NCCONG@DESKTOP-1F27F3C>
2019-01-26 10:11:46 +0700

    init

---
+++ src/main/java/cloud/csoft/com/search/SearchApplication.java
@@ -0,0 +1,19 @@
+package cloud.csoft.com.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+//@EnableSwagger2
+public class SearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/cloud/csoft/come/search/SearchApplicationTests.java
@@ -0,0 +1,17 @@
+package cloud.csoft.come.search;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SearchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
